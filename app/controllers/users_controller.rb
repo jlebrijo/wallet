1553f7eb0178d1419e54b5ef45c7e35665e07e23
Juan Lebrijo <juan@lebrijo.com>
2019-10-30 16:17:23 +0100

    List and create owners: Users, Stocks and Teams

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def create
+    @owner = User.new(user_params)
+    save_and_render_owner
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'wallets#index'
 
-  resources :wallets, only: [:index, :new]
+  resources :users, only: %i(new create)
+  resources :teams, only: %i(new create)
+  resources :stocks, only: %i(new create)
+  resources :wallets, only: %i(index new create)
+  resources :transactions, only: %i(new create)
 end
